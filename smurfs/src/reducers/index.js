c87acf64ad132e82e232f47a798cbe1333fbd1d4
Your Name <tfikrie@yahoo.com>
2019-07-19 20:01:44 -0400

    reducer function completed

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-import { GET_SMURFS_START, GET_SMURFS_SUCCESS, GET_SMURFS_FAILED} from './actions'
+import { GET_SMURFS_START, GET_SMURFS_SUCCESS, GET_SMURFS_FAILED} from '../actions'
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this */
  
@@ -21,3 +21,52 @@ import { GET_SMURFS_START, GET_SMURFS_SUCCESS, GET_SMURFS_FAILED} from './action
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+const rootReducer = (state = initialState, action) => {
+  switch(action.type){
+      case GET_SMURFS_START:
+          return {
+              ...state,
+              isLoading: true,
+          }
+ 
+      case GET_SMURFS_SUCCESS:
+          return {
+              ...state,
+              smurfs: action.payload,
+              isLoading: false,
+          }
+ 
+      case GET_SMURFS_FAILED:
+          return {
+              ...state,
+              isLoading: false,
+              error: action.payload.error,
+          }
+ 
+      case  POST_SMURFS_START: {
+          return {
+              ...state,
+              addingSmurf: true,
+          }
+      }
+      case  POST_SMURFS_SUCCESS: {
+          return {
+              ...state,
+              smurfs: action.payload,,
+              error: null,
+          }
+      }
+      case POST_SMURFS_FAILED: {
+          return {
+              ...state,
+              addingSmurf: false,
+              error: action.payload.error,
+          }
+      }
+ 
+    default:
+      return state
+  }
+ };
+ 
+ export default rootReducer;
